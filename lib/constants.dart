46a9a9c980d8ca3a087915aec10684609c7b314e
Aweroro Hadi <60360809+Aweroro@users.noreply.github.com>
2022-04-07 21:20:33 +0100

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+
+const kBottomContainerHeight = 80.0;
+const kInactiveCardColour = Color(0XFF111328);
+const kActiveCardColour = Color(0XFF1D1E33);
+const kBottomContainerColor = Color(0XFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0XFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 25.0,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0XFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 100.0,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
